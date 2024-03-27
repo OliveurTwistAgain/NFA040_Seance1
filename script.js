46c6dd3233e330a9eef0e1336e947c2067bf62f5
OliveurTwistAgain <165167631+OliveurTwistAgain@users.noreply.github.com>
2024-03-27 18:01:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+    $('.js-spanize').each(function() {
+        var text = $(this).text();
+        var spanizedText = '';
+        for (var i = 0; i < text.length; i++) {
+            spanizedText += '<span style="animation-delay:' + (i * 0.1) + 's">' + text[i] + '</span>';
+        }
+        $(this).html(spanizedText);
+    });
+});
