d7c7389ca3ced5c335211fd57cf23204bbc43d35
OliveurTwistAgain <165167631+OliveurTwistAgain@users.noreply.github.com>
2024-03-27 19:37:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,27 +10,19 @@
 });
 */
 
-/*-----
-Spanizer
-- Wraps letters with spans, for css animations
------*/
-(function($) {
-  var s,
-  spanizeLetters = {
-    settings: {
-      letters: $('.js-spanize'),
-    },
-    init: function() {
-      s = this.settings;
-      this.bindEvents();
-    },
-    bindEvents: function(){
-      s.letters.html(function (i, el) {
-        //spanizeLetters.joinChars();
-        var spanizer = $.trim(el).split("");
-        return '<span>' + spanizer.join('</span><span>') + '</span>';
-      });
-    },
-  };
-  spanizeLetters.init();
-})(jQuery);
+$(document).ready(function() {
+    $('.js-spanize').each(function() {
+        var text = $(this).text();
+        var spanizedText = '';
+        for (var i = 0; i < text.length; i++) {
+            spanizedText += '<span>' + text[i] + '</span>';
+        }
+        $(this).html(spanizedText);
+    });
+
+    // Animation des lettres
+    $('.js-spanize span').each(function(index) {
+        $(this).css('animation-delay', index * 0.1 + 's');
+    });
+});
+
